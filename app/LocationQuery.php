0b7c3339a66f2999ad85a052ab2e681852b31b3e
Schmar James <loyd.slj@gmail.com>
2015-06-22 22:59:48 -0400

    location query model

---
+++ app/LocationQuery.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LocationQuery extends Model
+{
+    public $table = 'location_query';
+    public $timestamps = false;
+}
