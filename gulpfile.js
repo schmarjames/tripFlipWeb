6e6b238695a8bfd948dc470adabeb6837a924c39
Schmar James <schmarjames@Mac-9.local>
2015-06-29 01:02:13 -0400

    bower and gulp installation

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,23 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.copy('vendor/bower_components/bootstrap/fonts/', 'public/fonts');
+    mix.less('app.less', 'resources/assets/build/app.css');
+    mix.styles([
+        'resources/assets/css/vendor/bootstrap.min.css',
+        'resources/assets/css/vendor/bootstrap-theme.min.css',
+        'assets/build/app.css'
+    ], 'public/css/app.css', 'resources/');
+    mix.copy(
+            'vendor/bower_components/bootstrap/dist/css/bootstrap.css.map',
+            'public/css/bootstrap.css.map'
+        );
+    mix.copy(
+            'vendor/bower_components/bootstrap/dist/css/bootstrap-theme.css.map',
+            'public/css/bootstrap-theme.css.map'
+        );
+    mix.scripts([
+        'resources/assets/js/vendor/angular.js',
+    ], 'public/js/site.js', 'resources/');
+
 });
