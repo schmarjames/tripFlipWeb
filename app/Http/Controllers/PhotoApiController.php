06b8b64118cd74390967ade43537a0c079236d1f
Schmar James <schmarjames@Mac.local>
2015-09-25 10:08:11 -0400

    optimize filter job functionality

--- app/Http/Controllers/PhotoApiController.php
+++ app/Http/Controllers/PhotoApiController.php
@@ -9,10 +9,18 @@ use App\Jobs\QueryPhotoData;
 use App\Jobs\FilterDataQueue;
 use App\LocationQuery;
 use App\TmpFlickrData;
+use App\AcceptedPhotos;
+use App\RejectedPhotos;
 use Carbon\Carbon;
 
 class PhotoApiController extends Controller
 {
+
+  protected $flickrEntryCount;
+  protected $flickrEntries;
+  protected $country;
+  protected $state_region;
+  protected $city;
 
     public function locationQuery(Request $request) {
       $locations = LocationQuery::all();
@@ -21,9 +29,76 @@ class PhotoApiController extends Controller
 
     public function filterData(Request $request) {
       $temps = TmpFlickrData::where('created_at', '<=', Carbon::now())->select('id')->get()->toArray();
-      $tmpIds = array_column($temps, 'id');
-      $this->dispatch(new FilterDataQueue($tmpIds));
+      $this->flickrEntries = array_column($temps, 'id');
+      //$this->dispatch(new FilterDataQueue($tmpIds));
+      foreach($this->flickrEntries as $flickrEntry) {
+          $entry = TmpFlickrData::find($flickrEntry);
+
+          $this->country      = $entry->country;
+          $this->state_region = $entry->state_region;
+          $this->city         = $entry->city;
+
+        //check each photo of this entry to see if its valid
+        $this->_sortPhotoCollection(unserialize($entry->response_data));
+      }
     }
+
+    /*
+     *	Place each photo entry in its appropriate table
+     *   based on its acceptance
+     *
+     *  @return void
+     */
+   protected function _sortPhotoCollection($photoArrs) {
+     foreach($photoArrs as $photoArr) {
+         $result;
+         // pass flickr url of photo to filter method
+         $result = $this->_filterPhoto($this->_photoUrl($photoArr));
+
+         // if photo is valid
+         // store in accepted table
+         if($result) {
+             AcceptedPhotos::create([
+                 'country' => $this->country,
+                 'state_region' => $this->state_region,
+                 'city' => $this->city,
+                 'photo_data' => json_encode($photoArr)
+             ]);
+         } else {
+             RejectedPhotos::create([
+                 'country' => $this->country,
+                 'state_region' => $this->state_region,
+                 'city' => $this->city,
+                 'photo_data' => json_encode($photoArr)
+             ]);
+         }
+     }
+   }
+
+   /*
+    *	Scans image to see if it is valid
+    *	Face Detection
+    *
+    *  @return bool
+    */
+   protected function _filterPhoto($url) {
+       // pass url to python script
+       return (bool)exec("python scanPhoto.py $url");
+   }
+
+     /*
+      *	Generate photo url
+      *
+      *  @return string
+      */
+   protected function _photoUrl($data) {
+     return sprintf('https://farm%s.staticflickr.com/%d/%d_%s.jpg',
+             $data['farm'],
+             $data['server'],
+             $data['id'],
+             $data['secret']
+         );
+   }
 
     /**
      * Display a listing of the resource.
