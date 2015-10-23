dcf4ae36254e2ea70b59d81c6c36c2f655834519
Schmar James <schmarjames@Mac.local>
2015-10-22 22:47:20 -0400

    user location collection query functionality

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -104,7 +104,6 @@ class GalleryController extends Controller
             }
           )
           ->toArray();
-
         $collection = Tfphotos::select('tfphotos.*', 'location_data.lat', 'location_data.long', 'countries.country', 'state_regions.state_region', 'cities.city', 'counties.county')
           ->join('location_data', 'tfphotos.location_id', '=', 'location_data.id')
           ->join('countries', 'tfphotos.country_id', '=', 'countries.id')
