c600fa4fa48d57f7b1ab8fcfeb9b234ec659fbd1
Schmar James <schmarjames@Mac.local>
2015-11-03 13:30:39 -0500

    include geo data resource for users current coords

---
+++ app/Http/Controllers/LocationApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Jobs\QueryPhotoData;
+use App\Jobs\FilterDataQueue;
+use App\LocationQuery;
+use App\TmpFlickrData;
+use Carbon\Carbon;
+use Log;
+
+
+class LocationApiController extends Controller {
+
+  public function handleLocation(Request $request) {
+    Log::info($request);
+
+
+  }
+
+}
