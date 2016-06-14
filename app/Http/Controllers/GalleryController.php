0965c83fb64a08675113c76025837c16973b35f6
Schmar James <loyd.slj@gmail.com>
2016-06-14 07:08:47 -0400

    modify user category collection query

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -228,7 +228,7 @@ class GalleryController extends Controller
         ->leftJoin('state_regions', 'tfphotos.state_region_id', '=', 'state_regions.id')
         ->leftJoin('cities' , 'tfphotos.city_id', '=', 'cities.id')
         ->leftJoin('counties', 'tfphotos.county_id', '=', 'counties.id')
-        ->whereIn('tfphotos.id', function($query) use($data) {
+        ->whereIn('tfphotos.id', function($query) use($data, $user) {
 
           $query
             ->from('likes')
