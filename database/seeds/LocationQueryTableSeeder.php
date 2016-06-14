5a4014d11da6636039477449ceb256589b98339e
Schmar James <loyd.slj@gmail.com>
2016-06-14 07:54:15 -0400

    modify location query seed data

--- database/seeds/LocationQueryTableSeeder.php
+++ database/seeds/LocationQueryTableSeeder.php
@@ -7,38 +7,38 @@ class LocationQueryTableSeeder extends Seeder {
     public function run() {
         DB::table('location_query')->insert(
             array(
-                /*array(
-                    "country"       => "spain",
-                    "state_region"  => "",
-                    "city"          => 'barcelona',
-                    "total_pages"   => 0,
-                    "current_page"  => 0
-                ),
-                array(
-                    "country"       => "greece",
-                    "state_region"  => "",
-                    "city"          => 'athens, aegean',
-                    "total_pages"   => 0,
-                    "current_page"  => 0
-                ),
-                array(
-                    "country"       => "turkey",
-                    "state_region"  => "",
-                    "city"          => 'istanbul',
-                    "total_pages"   => 0,
-                    "current_page"  => 0
-                ),
-                array(
-                    "country"       => "argentina",
-                    "state_region"  => "",
-                    "city"          => 'buenos, aires',
-                    "total_pages"   => 0,
-                    "current_page"  => 0
-                ),*/
                 array(
                     "country"       => "",
+                    "state_region"  => "Florida",
+                    "city"          => 'Naples',
+                    "total_pages"   => 0,
+                    "current_page"  => 0
+                ),
+                array(
+                    "country"       => "Colombia",
                     "state_region"  => "",
-                    "city"          => 'San Francisco',
+                    "city"          => "Medellin",
+                    "total_pages"   => 0,
+                    "current_page"  => 0
+                ),
+                array(
+                    "country"       => "Italy",
+                    "state_region"  => "",
+                    "city"          => 'Rome',
+                    "total_pages"   => 0,
+                    "current_page"  => 0
+                ),
+                array(
+                    "country"       => "Greece",
+                    "state_region"  => "",
+                    "city"          => "Mykonos",
+                    "total_pages"   => 0,
+                    "current_page"  => 0
+                ),
+                array(
+                    "country"       => "Czech Republic",
+                    "state_region"  => "",
+                    "city"          => "Prague",
                     "total_pages"   => 0,
                     "current_page"  => 0
                 )
